1063fe479dcca3019c338e9b0e7aed26a604ae0c
ghanglha <ghanglha23@gmail.com>
2023-10-09 11:58:41 +0530

    added cloned

---
+++ tina/config.js
@@ -0,0 +1,56 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "d3c30fb2-e81b-4160-a47b-d177e22cec3e", // Get this from tina.io
+  token: "53e867bb7568a70aa9199f8658e0ec8ce4bb6f74", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "./",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "./",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "_posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "layout",
+            label: "Layout",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+           {
+            type: "string",
+            name: "category",
+            label: "Category",
+          },
+        ],
+      },
+    ],
+  },
+});
